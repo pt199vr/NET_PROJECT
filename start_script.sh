8cc5b5d49c209af1afd8324d8e61c95cab3beca8
ChiaraBozz <chiarabozz00@gmail.com>
2024-06-21 17:37:32 +0200

    Remove

--- start_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup /usr/bin/python3 /home/univr/Desktop/NET_PROJECT/pi_camera.py > /home/univr/Desktop/images/your_script.log 2>&1 &
-echo $! > /home/univr/Desktop/images/your_script.pid
