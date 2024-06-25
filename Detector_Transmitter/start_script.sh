e9e095707e0bcf3c9f3aa579cfc1ceaea6d13bf9
Chiara Bozzini <104632380+ChiaraBozz@users.noreply.github.com>
2024-06-25 17:17:26 +0200

    Update start_script.sh

--- Detector_Transmitter/start_script.sh
+++ Detector_Transmitter/start_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup /usr/bin/python3 /home/univr/Desktop/NET_PROJECT/pi_camera.py > /home/univr/Desktop/images/your_script.log 2>&1 &
-echo $! > /home/univr/Desktop/images/your_script.pid
+nohup /usr/bin/python3 ~/Desktop/NET_PROJECT/Detector_Transmitter/insect.py > ~/Desktop/NET_PROJECT/insect.log 2>&1 &
+echo $! > ~/Desktop/NET_PROJECT/script.pid
