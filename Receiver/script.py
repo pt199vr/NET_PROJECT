695faf09026f3ab1a1c0aa2e7a622d37cda24113
pt199vr <pt99vr@gmail.com>
2024-06-28 11:55:31 +0200

    lora

--- Receiver/script.py
+++ Receiver/script.py
@@ -3,7 +3,7 @@ import os
 
 folder_path = "images/"
 
-#subprocess.run(['python', 'lora_rx.py'])
+subprocess.run(['python', 'lora_rx.py'])
 
 folder_contents = os.listdir(folder_path)
 while len(folder_contents)>0:
