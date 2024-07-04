4a6fc0cf75601e7069dcfb12fe0b0af1dc2035cc
Chiara Bozzini <104632380+ChiaraBozz@users.noreply.github.com>
2024-07-04 17:45:59 +0200

    Update script.py

--- Receiver/script.py
+++ Receiver/script.py
@@ -1,11 +1,6 @@
 import subprocess
-import os
-
-folder_path = "images/"
 
 subprocess.run(['python', 'lora_rx.py'])
 
-folder_contents = os.listdir(folder_path)
-while len(folder_contents)>0:
-	subprocess.run(['python', 'client.py'])
-	folder_contents = os.listdir(folder_path)
+subprocess.run(['python', 'client.py'])
+
