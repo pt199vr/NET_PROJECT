c90d879082caac3afda31c3c2190b7f6107c76b3
pt199vr <pt99vr@gmail.com>
2024-07-04 20:42:36 +0200

    fototrap

--- PC_CODE/sorter.py
+++ PC_CODE/sorter.py
@@ -2,28 +2,35 @@ import os
 import shutil
 
 # Directory containing the files
-folder_path = 'INSETTI/'
+folder_path = 'INSETTI'
 
-# Function to extract date from file name
-def extract_date(filename):
-    # Assuming the date is always in the format "YYYY_MM_DD" and at the same position in the file name
+# Function to extract date and fototrappola from file name
+def extract_info(filename):
     parts = filename.split('_')
     date_str = '_'.join(parts[3:6])  # Extracts the date part from the file name
-    return date_str
+    fototrappola = parts[0]  # Extracts the fototrappola identifier
+    return date_str, fototrappola
 
 # List all files in the directory
 files = os.listdir(folder_path)
 
 for file in files:
-    # Extract date from file name
-    date_str = extract_date(file)
+    # Extract date and fototrappola from file name
+    date_str, fototrappola = extract_info(file)
     
     # Create a new folder path for the date
-    new_folder_path = os.path.join(folder_path, date_str)
+    date_folder_path = os.path.join(folder_path, date_str)
     
-    # Check if the folder exists, if not create it
-    if not os.path.exists(new_folder_path):
-        os.makedirs(new_folder_path)
+    # Create a new folder path for the fototrappola inside the date folder
+    fototrappola_folder_path = os.path.join(date_folder_path, fototrappola)
     
-    # Move the file to the new folder
-    shutil.move(os.path.join(folder_path, file), os.path.join(new_folder_path, file))
+    # Check if the date folder exists, if not create it
+    if not os.path.exists(date_folder_path):
+        os.makedirs(date_folder_path)
+    
+    # Check if the fototrappola folder exists, if not create it
+    if not os.path.exists(fototrappola_folder_path):
+        os.makedirs(fototrappola_folder_path)
+    
+    # Move the file to the fototrappola folder inside the date folder
+    shutil.move(os.path.join(folder_path, file), os.path.join(fototrappola_folder_path, file))
